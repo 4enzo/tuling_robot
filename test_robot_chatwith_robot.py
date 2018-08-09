147e1739d3b8a52afc0ddacba369c803866fc1e9
4enzo <archer.enzo@hotmail.com>
2018-08-09 23:27:56 +0800

    linux下命令行显示登录二维码，增加指定群聊自动回复，增加关键字暂停/继续机器人自动回复

---
+++ test_robot_chatwith_robot.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2018-08-09
+
+@author: Enzo
+'''
+import tuling
+q1 = '你是谁谁'
+for i in range(10):
+    a2 = tuling.get_answer(q1)
+    print(a2)
+    a3 = tuling.get_answer(a2)
+
